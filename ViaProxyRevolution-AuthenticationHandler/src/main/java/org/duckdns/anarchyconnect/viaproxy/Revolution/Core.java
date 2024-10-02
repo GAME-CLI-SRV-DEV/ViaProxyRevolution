ad4b84be0c7bda2d1fb2e1c8e7b720be6c741962
Outfit8TSB <sunnybunnies09@naver.com>
2024-10-03 02:34:25 +0900

    Update Core.java

--- ViaProxyRevolution-AuthenticationHandler/src/main/java/org/duckdns/anarchyconnect/viaproxy/Revolution/Core.java
+++ ViaProxyRevolution-AuthenticationHandler/src/main/java/org/duckdns/anarchyconnect/viaproxy/Revolution/Core.java
@@ -1,3 +1,27 @@
 package org.duckdns.anarchyconnect.viaproxy;
 
-import net.raphimc.minecraftauth;
+import net.raphimc.minecraftauth.MinecraftAuth;
+import net.raphimc.minecraftauth.step.StepMsaDeviceCode;
+import net.raphimc.minecraftauth.session.JavaSession;
+import net.raphimc.minecraftauth.session.BedrockSession;
+
+public class main extends ViaProxyPlugin {
+
+String kickMessage = "Please enter the auth code at aka.ms/remoteconnect.";
+
+  
+      public void setCancelled(final boolean cancelled) {
+    @EventHandler
+    public void onPreconnect(PreconnectEvent event) {
+        // Logic to display kickscreen
+        String kickMessage = "Please enter the auth code at aka.ms/remoteconnect: JE: ";
+        if (event instanceof EventCancellable) {
+            EventCancellable cancellableEvent = (EventCancellable) event;
+            cancellableEvent.setCancelMessage(kickMessage + msaDeviceCode.getUserCode() + );
+            cancellableEvent.setCancelled(true);
+        }
+    }
+  
+  }
+
+  
